6e94608595c8addce1984bf36305c2ed9b9a9e22
zl3721 <zl3721@ic.ac.uk>
2023-01-27 09:06:03 +0000

    Add primes

--- math_utils/primes.py
+++ math_utils/primes.py
@@ -1,5 +1,8 @@
 def isprime(n):
-    for i in range(2,n):
-        if n%i == 0:
-            return False
-    return True
+    if n == 1 :
+        return False
+    else:    
+        for i in range(2,n):
+            if n%i == 0:
+                return False
+        return True
